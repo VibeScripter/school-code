35e708016838413caae748bc992aa1773701190b
Catherine De Mesa <59064455+xCoderSai@users.noreply.github.com>
2021-07-19 19:30:32 -0500

    CH 2 - C# I

---
+++ Program2_3/Program2_3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Eight of Diamonds";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Two of Clubs";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "King of Spades";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Ace of Spades";
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Joke of Black";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
